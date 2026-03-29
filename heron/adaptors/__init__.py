494a5d830e4a8543d96b97b71b155732b5ddd8e8
criss-wang <djj87018507@gmail.com>
2026-03-29 02:17:54 -0700

    Simple init import fix in adaptors folder

--- heron/adaptors/__init__.py
+++ heron/adaptors/__init__.py
@@ -2,7 +2,9 @@
 
 try:
     from heron.adaptors.rllib import RLlibBasedHeronEnv
+    from heron.adaptors.rllib_runner import HeronEnvRunner
+    from heron.adaptors.rllib_module_bridge import RLlibModuleBridge
 except ImportError:
     pass
 
-__all__ = ["RLlibBasedHeronEnv"]
+__all__ = ["RLlibBasedHeronEnv", "HeronEnvRunner", "RLlibModuleBridge"]
